3699397fda7ed5e0ba9678894ee351dfe9395b0b
Ruwan Geeganage <ruwan@Ruwans-MBP-2.fritz.box>
2023-03-02 17:55:12 +0100

    added purge tests

--- e2e/tests/purge.spec.ts
+++ e2e/tests/purge.spec.ts
@@ -18,14 +18,42 @@ async function apiPost(data: unknown): Promise<string> {
   return id;
 }
 
+async function apiGetStatus(id: string): Promise<number> {
+  const response = await fetch(`${apiPath}/record/${id}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response.status;
+}
+
 describe('Purge', () => {
+  it('should delete record once it has been accessed', async () => {
+    const id = await apiPost({
+      content: 'test',
+      expireIn: { value: 1, unit: 'minutes' },
+    });
+
+    const responseCodeGet1 = await apiGetStatus(id);
+    expect(responseCodeGet1).toBe(200);
+
+    const responseCodeGet2 = await apiGetStatus(id);
+    expect(responseCodeGet2).toBe(404);
+  });
+
   it('should clean expired data', async () => {
     const id = await apiPost({
       content: 'test',
       expireIn: { value: 1, unit: 'minutes' },
     });
 
-    console.log(id);
-    expect(true).toBe(true);
+    // Wait for 2 minutes
+    await new Promise((r) => setTimeout(r, 12_0000));
+    const responseCode = await apiGetStatus(id);
+
+    expect(responseCode).toBe(404);
   });
 });
